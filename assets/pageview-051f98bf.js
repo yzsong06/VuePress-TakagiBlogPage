2d1a47fecceeb642e2ab5902094ce6a95879315a
yzsong06 <yzsong06@outlook.com>
2023-08-09 07:18:42 +0000

    Update main to output generated at 0c373bd

---
+++ assets/pageview-051f98bf.js
@@ -0,0 +1,1 @@
+import{K as e}from"./app-508476bd.js";const a=()=>e({serverURL:"https://waline-1-j6551192.deta.app"});export{a as updatePageview};
--- assets/pageview-9e3dfb60.js
+++
@@ -1,1 +0,0 @@
-import{K as e}from"./app-c1d67c92.js";const a=()=>e({serverURL:"https://waline-1-j6551192.deta.app"});export{a as updatePageview};
